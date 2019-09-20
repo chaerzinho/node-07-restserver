5e83bb5f0d0cdd2169f8f4d10f2a6eab2796fd9a
chaerzinho <pchaer@gmail.com>
2019-09-20 15:17:28 -0300

    get post y acceso a mongodb

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require('./config/config');
 
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
+const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -10,39 +11,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function (req, res) {
-  res.json('get usuario');
-});
+app.use( require('./routes/usuario'));
 
-app.post('/usuario', function (req, res) {
+mongoose.connect('mongodb://localhost:27017/cafe',
+                 { useNewUrlParser: true, useUnifiedTopology: true },
+                 (err, res) => {
 
-  let body = req.body;
+  if ( err ) throw err;
 
-  if (body.nombre === undefined ) {
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    })
-  } else {
-    res.json({
-      persona: body
-    });
-  }
+  console.log('Base de datos ONLINE');
 
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-  let id = req.params.id;
-
-  res.json({
-    id
-  });
-
-});
-
-app.delete('/usuario', function (req, res) {
-  res.json('delete usuario');
 });
 
 app.listen(process.env.PORT, () => {
